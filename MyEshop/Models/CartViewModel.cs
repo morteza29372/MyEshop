728cf0d5ca536e2d3b0bbd4e723d519946d7a305
Morteza <morteza29372@yahoo.com>
2022-07-31 21:56:43 +0430

    addviewmodel

---
+++ MyEshop/Models/CartViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace MyEshop.Models
+{
+    public class CartViewModel
+    {
+
+        public CartViewModel()
+        {
+            cartItems = new List<CartItem>();
+        }
+        public List<CartItem> cartItems { get; set; }
+
+        public decimal OrderTotal { get; set; }
+    }
+}
