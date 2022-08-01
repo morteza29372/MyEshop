c1284ba11e6264f7949c169341dda9b604394e77
TabanSoft <info@tabansoft.ir>
2022-08-01 15:54:15 +0430

    29

---
+++ MyEshop/Data/MyEshopContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyEshop.Models;
+
+namespace MyEshop.Data
+{
+    public class MyEshopContext:DbContext 
+    {
+        public MyEshopContext(DbContextOptions<MyEshopContext> options):base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- MyEshop/Startup.cs
+++ MyEshop/Startup.cs
@@ -23,6 +23,7 @@ namespace MyEshop
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
