4e7180df97fbb8cb0c8dc18ea2d007301f3a91ff
TabanSoft <info@tabansoft.ir>
2022-08-04 13:53:25 +0430

    AddproductController

---
+++ MyEshop/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyEshop.Data;
+
+namespace MyEshop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private MyEshopContext _context;
+
+        public ProductController(MyEshopContext context)
+        {
+            _context = context;
+        }
+        [Route("Group/{id}/{name}")]
+        public IActionResult ShowProductByGroupId(int id,string name)
+        {
+            return View();
+        }
+    }
+}
