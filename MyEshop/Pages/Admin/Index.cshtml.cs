4652910544948b79f8f40a018b2c911ffb190b6b
Morteza <morteza29372@yahoo.com>
2022-08-09 20:30:46 +0430

    AddindexAdmin

--- MyEshop/Pages/Admin/Index.cshtml.cs
+++ MyEshop/Pages/Admin/Index.cshtml.cs
@@ -1,12 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using MyEshop.Data;
+using MyEshop.Models;
 
 namespace MyEshop.Pages.Admin
 {
     public class IndexModel : PageModel
     {
+        private MyEshopContext _context;
+
+        public IndexModel(MyEshopContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Product> Products { get; set; }
+
         public void OnGet()
         {
+            Products = _context.Products.Include(o=> o.Item);
+        }
+
+        public void OnPost()
+        {
+
         }
     }
 }
