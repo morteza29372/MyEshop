eae116472617ce641cf3d0a69ede53d03701f7ce
Morteza <morteza29372@yahoo.com>
2022-08-10 22:05:44 +0430

    update .net

---
+++ MyEshop/Pages/Admin/ManageUser/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MyEshop.Data;
+using MyEshop.Models;
+
+namespace MyEshop.Pages.Admin.ManageUser
+{
+    public class CreateModel : PageModel
+    {
+        private readonly MyEshop.Data.MyEshopContext _context;
+
+        public CreateModel(MyEshop.Data.MyEshopContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Users Users { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Users.Add(Users);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
